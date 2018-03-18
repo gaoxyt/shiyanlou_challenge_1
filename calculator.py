ba773a12cd96bf9eab2c25f3b36ea93a1b326431
gaoxyt <gaoxyt@163.com>
2018-03-18 23:23:00 +0800

    salary_after_tax

--- calculator.py
+++ calculator.py
@@ -2,25 +2,46 @@
 
 import sys
 
-tax      = [0,1500,4500,9000,35000,55000,80000]
-tax_rate = [0,0.03,0.1,0.2,0.25,0.3,0.35,0.45]
-sub_num  = [0,0,105,555,1005,2755,5505,13505]
+def calc_salary(salary):
+	tax = [0,1500,4500,9000,35000,55000,80000]
+	tax_rate = [0,0.03,0.1,0.2,0.25,0.3,0.35,0.45]
+	sub_num = [0,0,105,555,1005,2755,5505,13505]
+	insurance = (0.08,0.02,0.005,0.000,0.000,0.06)
 
+	ins_pay = 0
+	for insurance_temp in insurance:
+		ins_pay += salary * insurance_temp
 
-if __name__ == '__main__':
-	try:
-		salary = int(sys.argv[1])
-	except:
-		print("Parameter Error")
-		exit() 
-	salary = salary - 3500
+	salary_temp = salary - ins_pay -  3500
 	i = 0
 	for tmp in tax:
-		if salary <= tmp:
-			tax_pay = salary * tax_rate[i] - sub_num[i]
+		if salary_temp <= tmp:
+			tax_pay = salary_temp * tax_rate[i] - sub_num[i]
 			break;
 		else:
 			i = i + 1
 
-	print(format(tax_pay,".2f"))
+	return format(salary - tax_pay - ins_pay,".2f")
+
+
+if __name__ == '__main__':
+	arg_dict = {}
+	i = 1
+	if len(sys.argv) < 2:
+		print("Parameter Error")
+		exit()
+	for arg in sys.argv:
+		try:
+			if i != 1:
+				arg_list = arg.split(':')
+				arg_dict[arg_list[0]] = int(arg_list[1])
+			i += 1;
+			#print(arg_dict)
+		except:
+			print("Parameter Error")
+			exit() 
+	
+	for key,value in arg_dict.items():
+		print(key,calc_salary(value))
+		
 	exit()
